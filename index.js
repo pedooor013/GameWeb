9a6d44aef2badeb9292df118501e2ec49d54df8c
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-11-15 12:11:16 -0300

    feat:finalizacao do projeto de desenvolvimento web -  jogo em 3 etapas funcionando

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const senhaCofre = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+const senhaCofre = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
 const arrSenhaCofre = senhaCofre.toString().split('');
 
 const firstChallenge = document.getElementById('first-challenge');
 const secondChallenge = document.getElementById('second-challenge');
 const thirdChallenge = document.getElementById('third-challenge');
-const fourthChallenge = document.getElementById('fourth-challenge');
-const fifthChallenge = document.getElementById('fifth-challenge');
-
+const lastResponse = document.getElementById('last-response');
 
 const resposta = document.getElementById('resposta');
 
@@ -51,6 +49,61 @@ async function sendResponseFirstQuestion(){
         window.alert("Resposta incorreta! Voltando ao inicio!!!")
         location.reload(true);
     }
-
 }
+
+async function sendResponseSecondQuestion(){
+    var radioElements = document.getElementsByName('company');
+    var selectElement;
+    for(i = 0; i < radioElements.length; i++){
+        if(radioElements[i].checked){
+            selectElement = radioElements[i].value;
+        }
+    }
+
+    if(selectElement == "Google"){
+        window.alert("Resposta Correta!");
+        document.getElementById('resposta-second-question').classList.remove('d-none');
+        document.getElementById('resposta-second-question').innerHTML = `${arrSenhaCofre[1]}`;
+        setTimeout(() => {
+            secondChallenge.classList.add('d-none');
+            thirdChallenge.classList.remove('d-none');
+        }, 1500);
+    }else{
+        window.alert("Resposta incorreta! Voltando ao inicio!!!")
+        location.reload(true);
+    }
+}
+
+function sendResponseThirdQuestion(){
+    const response = document.getElementById('first-programming-language-response').value.toUpperCase();
+
+    if(response == 'FORTRAN'){
+        window.alert("Resposta Correta!");
+        document.getElementById('response-third-question').classList.remove('d-none');
+        document.getElementById('response-third-question').innerHTML = `${arrSenhaCofre[2]}`;
+        setTimeout(() => {
+            thirdChallenge.classList.add('d-none');
+            lastResponse.classList.remove('d-none');
+        }, 1500);
+    }else{
+        window.alert("Resposta incorreta! Voltando ao inicio!!!")
+        location.reload(true);
+    }
+}
+
+function checkPassword(){
+    const firstNum = document.getElementById('first-response').value;
+    const secondNum = document.getElementById('second-response').value;
+    const thirdNum = document.getElementById('third-response').value;
+
+    if((firstNum == arrSenhaCofre[0]) && (secondNum == arrSenhaCofre[1]) && (thirdNum == arrSenhaCofre[2])){
+        window.alert("Resposta Correta!");
+        setTimeout(() => {
+            lastResponse.classList.add('d-none');
+            document.getElementById('final').classList.remove('d-none');
+        }, 1500);
+    }else{
+        console.log(arrSenhaCofre);
+    }
+} 
 
